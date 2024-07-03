7fe7eb3b4d983e44e101fb29ca8a66c9114c6749
riasc <richard.schaefer@zoho.com>
2024-07-02 23:44:14 -0500

    reimplementation splitNode

--- src/IBPTree.cpp
+++ src/IBPTree.cpp
@@ -33,6 +33,29 @@ void IBPTree::insert(std::string key, T& data) {
     }
 }
 
+void IBPTree::splitNode(Node* parent, int index) {
+    // std::cout << "Splitting node " << parent->keysToString() << " at index " << index << "\n";
+    Node* child = parent->getChild(index);
+    Node* newChild = new Node(this->order);
+    int mid = ((this->order+2-1)/2); // value for order=6
+
+    newChild->isLeaf = child->isLeaf;
+    newChild->keys.assign(child->keys.begin() + mid, child->keys.end());
+    child->keys.resize(this->order-1);
+
+    // update parent
+    parent->children.insert(parent->children.begin() + index + 1, newChild);
+    parent->keys.insert(parent->keys.begin() + index, {child->calcNewKey(), nullptr});
+
+    if(child->isLeaf) {
+        newChild->next = child->next;
+        child->next = newChild;
+    } else {
+        newChild->children.assign(child->children.begin() + mid, child->children.end());
+        child->children.resize(mid + 1);
+    }
+}
+
 
 
 
