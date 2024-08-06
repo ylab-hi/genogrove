44291635952ec13d8dee6cfc5b1077505285cf8e
riasc <richard.schaefer@zoho.com>
2024-08-06 00:56:05 -0500

    added leftOf function to check if interval is non-overlapping left of first interval in key

--- include/Interval.hpp
+++ include/Interval.hpp
@@ -1,8 +1,37 @@
-//
-// Created by Richard Albin Schaefer on 7/27/24.
-//
-
 #ifndef GENGROVE_INTERVAL_HPP
 #define GENGROVE_INTERVAL_HPP
+
+// Standard
+#include <cstddef>
+#include <string>
+
+class Interval {
+    public:
+        Interval();
+        Interval(size_t start, size_t end);
+        ~Interval();
+
+        /*
+         * @brief operator overloading that compares are non-overlapping (left-most end < right-most start)
+         */
+        bool operator<(const Interval& other) const;
+        bool operator>(const Interval& other) const;
+
+        bool overlap(const Interval& other) const;
+        /*
+         * @brief check if provided interval is left of class interval (no overlaps)
+         */
+        bool leftOf(const Interval& other) const;
+
+        // getter & setter
+        size_t getStart() const;
+        void setStart(size_t start);
+        size_t getEnd() const;
+        void setEnd(size_t end);
+
+    private:
+        size_t start;
+        size_t end;
+};
 
 #endif //GENGROVE_INTERVAL_HPP
