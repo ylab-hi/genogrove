e903e245e73ca541ffb6661b02083a8bac9bd0b2
riasc <richard.schaefer@zoho.com>
2024-08-09 16:59:27 -0500

    refactoring headers placed to include/genogrove

--- tests/IBPTreeTestSuite.cpp
+++ tests/IBPTreeTestSuite.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "IBPTree.hpp"
+#include "genogrove/IBPTree.hpp"
 
 #include <stdlib.h>
 
@@ -10,38 +10,49 @@ TEST(IBPTreeTestSuite, CreateIBPTree) {
     std::vector<std::pair<Interval, std::shared_ptr<void>>> intervals;
 
     intervals.push_back({{0, 10}, std::make_shared<int>(rand() % 100)});
-    for(int i=11; i < 10000; i+=10) {
+    for(int i=11; i < 10000000; i+=10) {
         intervals.push_back({{static_cast<size_t>(i), static_cast<size_t>(i+9)},
                              std::make_shared<int>(rand() % 100)});
     }
 
-    std::cout << "#### Inserting data into the tree ####\n";
+    auto startInsertion = std::chrono::steady_clock::now();
 
     // insert data into the tree
     std::vector<std::shared_ptr<void>> dataVector;
     for(auto& intvl : intervals) { // insert data
         Key key{intvl.first, intvl.second};
-        std::cout << "Inserting key: " << key.getInterval().getStart() << "," << key.getInterval().getEnd();
-        std::cout << " with the data: " << *std::static_pointer_cast<int>(intvl.second) << "\n";
+//        std::cout << "Inserting key: " << key.getInterval().getStart() << "," << key.getInterval().getEnd();
+//        std::cout << " with the data: " << *std::static_pointer_cast<int>(intvl.second) << "\n";
         tree.insert("chr1", key);
     }
+    auto endInsertion = std::chrono::steady_clock::now();
+    std::chrono::duration<double> insertionDuration = endInsertion - startInsertion;
+    std::cout << "Time taken for insertion: " << insertionDuration.count() << " seconds\n";
 
     std::cout << "#### Searching for data within the tree ####\n";
 
+    auto startSearch = std::chrono::steady_clock::now();
+
     // search for data within the tree
     for(auto& intvl : intervals) {
-        std::cout << "Searching for interval [" << intvl.first.getStart() << "," << intvl.first.getEnd() << "]";
-        std::cout << " with the data: " << *std::static_pointer_cast<int>(intvl.second);
+//        std::cout << "Searching for interval [" << intvl.first.getStart() << "," << intvl.first.getEnd() << "]";
+//        std::cout << " with the data: " << *std::static_pointer_cast<int>(intvl.second);
         std::vector<std::shared_ptr<void>> searchResult = tree.search("chr1", intvl.first);
 
-        std::cout << " -> search result: ";
-        for(auto& data : searchResult) {
-            std::cout << *std::static_pointer_cast<int>(data) << " ";
-        }
-        std::cout << "\n";
+//        std::cout << " -> search result: ";
+//        for(auto& data : searchResult) {
+//            std::cout << *std::static_pointer_cast<int>(data) << " ";
+//        }
+//        std::cout << "\n";
         EXPECT_EQ(searchResult.size(), 1) << "The search result for interval [" << intvl.first.getStart() << ","
                                           << intvl.first.getEnd() << "] was not correct";
         EXPECT_EQ(*std::static_pointer_cast<int>(searchResult[0]), *std::static_pointer_cast<int>(intvl.second))
             << "The search result for interval [" << intvl.first.getStart() << "," << intvl.first.getEnd() << "] was not correct";
     }
+    auto endSearch = std::chrono::steady_clock::now();
+    std::chrono::duration<double> searchDuration = endSearch - startSearch;
+    std::cout << "Time taken for search: " << searchDuration.count() << " seconds\n";
+
+
+    std::cout << "done\n";
 }
