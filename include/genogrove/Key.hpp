3f68ad2c79df47eb9ed5dc5eb123ceff71a95da9
riasc <richard.schaefer@zoho.com>
2024-08-21 15:35:40 -0500

    added AnyBase to allow genogrove to accept any database without type cast

--- include/genogrove/Key.hpp
+++ include/genogrove/Key.hpp
@@ -12,11 +12,42 @@
 namespace genogrove {
     class Interval; // forward declaration
 
+    class AnyBase {
+        virtual ~AnyBase() = default;
+        virtual std::string getTypeName() const = 0;
+    };
+
+    template<typename T>
+    class AnyType : public AnyBase {
+        private:
+            T data;
+
+        public:
+            AnyType(T&& data) : data(std::move(data)) {}
+
+            const T& getData() const { return data; }
+            T& getDate() { return data; }
+
+            std::string getTypeName() const override {
+                return typeid(T).name();
+            }
+    };
+
     class Key {
     public:
         Key();
         Key(Interval interval);
-        Key(Interval interval, std::shared_ptr<void> data);
+        template<typename T>
+        Key(Interval intvl, T&& data) {
+            // ensure that T is always a non-referencen type
+            using DataType = typename std::decay<T>::type;
+
+            // create a new data element
+            this->interval = intvl;
+            this->data = std::make_shared<typename std::decay<T>::type>(std::forward<T>(data));
+            this->singleLink = nullptr;
+            this->multiLink = std::vector<Key*>();
+        }
         ~Key();
 
         bool operator<(const Key& other) const;
