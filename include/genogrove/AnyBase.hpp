fff3a065ebe5763b4446a0866dcdb9430bb6fb83
riasc <richard.schaefer@zoho.com>
2024-08-27 12:17:53 -0500

    outsourced TypeErasure for readabilility

---
+++ include/genogrove/AnyBase.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Richard Albin Schaefer on 8/27/24.
+//
+
+#ifndef GENOGROVE_ANYBASE_HPP
+#define GENOGROVE_ANYBASE_HPP
+
+#endif //GENOGROVE_ANYBASE_HPP
---
+++ include/genogrove/AnyType.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Richard Albin Schaefer on 8/27/24.
+//
+
+#ifndef GENOGROVE_ANYTYPE_HPP
+#define GENOGROVE_ANYTYPE_HPP
+
+#endif //GENOGROVE_ANYTYPE_HPP
