ee66c05b62f44b22a196b645c00331ba1a958b42
riasc <richard.schaefer@zoho.com>
2024-08-28 15:47:22 -0500

    changed return types for cast and removed required typeIndex

--- include/genogrove/TypeRegistry.hpp
+++ include/genogrove/TypeRegistry.hpp
@@ -13,13 +13,12 @@
 #include <memory>
 
 // Class
-#include "AnyBase.hpp"
 #include "AnyType.hpp"
 
 namespace genogrove {
     class AnyBase;
 
-    using castFunction = std::function<void(const std::shared_ptr<AnyBase>&)>;
+    using castFunction = std::function<std::any(const std::shared_ptr<AnyBase>&)>;
 
     class TypeRegistry {
     public:
@@ -33,58 +32,25 @@ namespace genogrove {
 
         template<typename T>
         static void registerType() {
-            if (castFunctions.find(typeid(T)) == castFunctions.end()) {
-                castFunctions[typeid(T)] = [](const std::shared_ptr<AnyBase>& obj) {
-                    auto castedObj = std::dynamic_pointer_cast<AnyType<T>>(obj);
+            std::type_index typeIndex = typeid(T);
+            if (castFunctions.find(typeIndex) == castFunctions.end()) {
+                castFunctions[typeIndex] = [](const std::shared_ptr<AnyBase>& obj) -> std::any {
+                    auto castedObj = std::dynamic_pointer_cast<
+                            AnyType<typename std::remove_reference<T>::type>>(obj);
                     if(!castedObj) {
                         std::cerr << "Failed to cast object to type " << typeid(T).name() << std::endl;
                     }
+                    return std::any(castedObj->getData());
                 };
             }
         }
 
-        static void cast(const std::shared_ptr<AnyBase>& obj);
+        static std::any cast(const std::shared_ptr<AnyBase>& obj, std::type_index typeIndex);
 
     private:
         TypeRegistry() = default;
         static std::unordered_map<std::type_index, castFunction> castFunctions;
     };
 }
-
-
-
-//            template<typename T>
-//            static void registerType(const std::string &typeName) {
-//                auto it = typeMap.find(typeid(T));
-//                if(it != typeMap.end()) {
-//                    throw std::runtime_error("Type already registered");
-//                }
-//                typeMap[typeid(T)] = typeName;
-//                processFunctions[typeid(T)] = [](const std::shared_ptr<AnyBase>& obj) {
-//                    auto castedObj = std::dynamic_pointer_cast<T>(obj);
-//                    if(!castedObj) {
-//                        std::cerr << "Failed to cast object to type " << typeid(T).name() << std::endl;
-//                    }
-//                };
-//            }
-//
-//            static void process(const std::shared_ptr<AnyBase>& obj) {
-//                auto it = processFunctions.find(typeid(*obj));
-//                if(it != processFunctions.end()) {
-//                    it->second(obj);
-//                } else {
-//                    std::cerr << "Type not registered " << std::endl;
-//                }
-//            }
-//
-//        private:
-//            static std::unordered_map<std::type_index, std::string> typeMap;
-//            static std::unordered_map<std::type_index, std::function<void(const std::shared_ptr<AnyBase>&)>> processFunctions;
-//    };
-//
-//    std::unordered_map<std::type_index, std::string> TypeRegistry::typeMap;
-//    std::unordered_map<std::type_index, std::function<void(const std::shared_ptr<AnyBase>&)>> TypeRegistry::processFunctions;
-//}
-
 
 #endif //GENOGROVE_TYPEREGISTRY_HPP
