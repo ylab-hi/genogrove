fc80d0fad6a438e8cc613571730bedc9529e2a00
riasc <richard.schaefer@zoho.com>
2024-09-03 21:23:23 -0400

    changed creating typeregistry within key creation

--- include/genogrove/Key.hpp
+++ include/genogrove/Key.hpp
@@ -14,22 +14,42 @@
 #include "TypeRegistry.hpp"
 #include "AnyType.hpp"
 
+/**
+ * @file Key.hpp
+ */
 namespace genogrove {
+    // \brief forward declaration of the Interval class
     class Interval; // forward declaration
 
+    /**
+     * @brief The Key class
+     * @details The Key class is used to store the Key of the node in the IBPTree.
+     */
     class Key {
     public:
+        /**
+         * @brief Default constructor
+         */
         Key();
+        /**
+         * @brief Constructor for a key with an interval
+         * @param Interval of the key
+         */
         Key(Interval interval);
+        /**
+         * @brief Constructor of a key with an Interval and (arbitrary) data element
+         * @param Interval of the key
+         * @param data element of the key
+         */
         template<typename T>
         Key(Interval intvl, T&& data) :
             interval(intvl),
             data(std::make_shared<AnyType<std::decay_t<T>>>(data)),
             singleLink(nullptr),
-            multiLink(std::vector<Key*>()) {
-                // register the type of the data with the TypeRegistry
-                genogrove::TypeRegistry::registerType<T>();
-            }
+            multiLink(std::vector<Key*>()) {}
+        /**
+         * @brief Destructor of the Key class
+         */
         ~Key();
 
         bool operator<(const Key& other) const;
