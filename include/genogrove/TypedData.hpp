613c3640f7828ab7e71927e5a3ee56f363b2fbd0
riasc <richard.schaefer@zoho.com>
2024-09-06 17:20:44 -0400

    updated typed data structure to include all types

--- include/genogrove/TypedData.hpp
+++ include/genogrove/TypedData.hpp
@@ -7,8 +7,21 @@ class TypedData {
         TypedData(T data) : data(data) {}
         ~TypedData() {}
 
+        T getData() { return this->data; }
+
+        void addData
+
     private:
+        std::unordered_map<std::type_index, TypedData> typeMap;
+
+
         T data;
 };
 
 #endif //GENOGROVE_TYPEDDATA_HPP
+
+class AnyTypedData {
+private:
+    std::unordered_map<std::type_index, std::vector<TypedData>>
+
+};
