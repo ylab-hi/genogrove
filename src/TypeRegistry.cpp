3ebbf829e230a0dde912b3be82136ff1aeadd291
riasc <richard.schaefer@zoho.com>
2024-09-23 01:04:46 +0200

    explicitely declaration and nothing else

--- src/TypeRegistry.cpp
+++ src/TypeRegistry.cpp
@@ -1,26 +1,6 @@
 #include "genogrove/TypeRegistry.hpp"
 
 namespace genogrove {
-    // static members
-    std::unordered_map<std::type_index, castFunction> TypeRegistry::castFunctions;
-    std::unordered_map<std::type_index, std::string> TypeRegistry::typeNames;
-
-//    template<typename T>
-//    static void TypeRegistry::registerType() {
-//        if(castFunctions.find(typeid(T)) == castFunctions.end()) {
-//            castFunctions[typeid(T)] = [](const std::shared_ptr<AnyBase>& obj) {
-//                auto castedObj = std::dynamic_pointer_cast<AnyType<T>>(obj);
-//            };
-//        }
-//    }
-
-    std::any TypeRegistry::cast(const std::shared_ptr<AnyBase>& obj, std::type_index typeIndex) {
-        auto it = castFunctions.find(typeIndex);
-        if(it != castFunctions.end()) {
-            return it->second(obj);
-        } else {
-            std::cerr << "Type not registered " << std::endl;
-            return std::any();
-        }
-    }
+    std::unordered_map<std::type_index, genogrove::castFunction> genogrove::TypeRegistry::castFunctions;
+    std::unordered_map<std::type_index, std::string> genogrove::TypeRegistry::typeNames;
 }
