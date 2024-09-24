e807a1ba3ff5c04ec6ee55e9bd2f1af82d086696
riasc <richard.schaefer@zoho.com>
2024-09-25 01:08:28 +0200

    restore actual testsuite

--- tests/IBPTreeTestSuite.cpp
+++ tests/IBPTreeTestSuite.cpp
@@ -21,8 +21,7 @@ TEST(IBPTreeTestSuite, CreateIBPTree) {
     auto startInsertion = std::chrono::steady_clock::now();
     std::cout << "#### Inserting data into the tree ####\n";
     for(auto& intvl : intervals) {
-        genogrove::Key key{intvl.first, intvl.second};
-        tree.insert("chr1", key);
+        tree.insertData("chr1", intvl.first, intvl.second);
     }
     auto endInsertion = std::chrono::steady_clock::now();
     std::chrono::duration<double> insertionDuration = endInsertion - startInsertion;
@@ -36,86 +35,75 @@ TEST(IBPTreeTestSuite, CreateIBPTree) {
         EXPECT_EQ(searchResult.size(), 1) << "The search result for interval [" << intvl.first.getStart()
                                           << "," << intvl.first.getEnd() << "] was not correct (count mismatch)";
         // cast the data to the correct type
-        std::type_index dataTypeName = searchResult[0]->getDataType();
-        int typed = genogrove::TypeRegistry::cast(searchResult[0], dataTypeName);
+//        std::type_index dataTypeName = searchResult[0]->getDataType();
+        int typed = genogrove::TypeRegistry::cast<int>(searchResult[0]);
+        EXPECT_EQ(typed, intvl.second) << "The search result for interval ["
+            << intvl.first.getStart() << "," << intvl.first.getEnd() << "] was not correct (data mismatch)";
 
-
-
-//        std::any dataTypes = genogrove::TypeRegistry::cast(searchResult[0], dataTypeName);
-//        EXPECT_EQ(std::any_cast<int>(dataTypes), intvl.second) << "The search result for interval ["
-//            << intvl.first.getStart() << "," << intvl.first.getEnd() << "] was not correct (data mismatch)";
-
-
-
-
-
-
-//        EXPECT_EQ(std::any_cast<int>(searchResult[0]), intvl.second) << "The search result for interval ["
-//            << intvl.first.getStart() << "," << intvl.first.getEnd() << "] was not correct (data mismatch)";
     }
-//    auto endSearch = std::chrono::steady_clock::now();
-//    std::chrono::duration<double> searchDuration = endSearch - startSearch;
-//    std::cout << "Time taken for search: " << searchDuration.count() << " seconds\n\n";
+    auto endSearch = std::chrono::steady_clock::now();
+    std::chrono::duration<double> searchDuration = endSearch - startSearch;
+    std::cout << "Time taken for search: " << searchDuration.count() << " seconds\n\n";
 }
 
 
 TEST(IBPTreeTestSuite, benchmark) {
-
-    std::vector<int> orders = {20};
-
-    std::vector<std::string> unsorted = {
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_1K_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_10K_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_100K_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_1M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_2M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_3M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_4M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_5M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_6M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_7M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_8M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_9M_sorted.bed",
-            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_10M_sorted.bed"
-    };
-
-    genogrove::TypeRegistry::registerType<int>("Int");
-
-
-    std::cout << "unsorted" << std::endl;
-    for(int i=0; i < orders.size(); i++) {
-        std::cout << orders[i] << std::endl;
-
-        for(int j=0; j < unsorted.size(); j++) {
-            auto startInsertion = std::chrono::steady_clock::now();
-            genogrove::IBPTree tree(orders[i]);
-            std::cout << unsorted[j] << std::endl;
-            std::ifstream infile(unsorted[j]);
-            std::string line;
-            while(std::getline(infile, line)) {
-                std::istringstream iss(line);
-                std::string chrom;
-                int start, end, data;
-                iss >> chrom >> start >> end >> data;
-                genogrove::Interval intvl{size_t(start), size_t(end)};
-                genogrove::Key key{intvl, data};
-                tree.insert(chrom, key);
-            }
-            // time the insertion
-            auto endInsertion = std::chrono::steady_clock::now();
-            std::chrono::duration<double> insertionDuration = endInsertion - startInsertion;
-            std::cout << "Time taken for insertion: " << insertionDuration.count() << " seconds\n\n";
-        }
-    }
-
-
-    // time the search
-
-
-
-
-
-
+//
+//    std::vector<int> orders = {20};
+//
+//    std::vector<std::string> unsorted = {
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_1K_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_10K_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_100K_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_1M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_2M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_3M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_4M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_5M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_6M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_7M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_8M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_9M_sorted.bed",
+//            "/Users/sej9799/Documents/projects/gengrove/tests/bedfiles/bed_10M_sorted.bed"
+//    };
+//
+//    genogrove::TypeRegistry::registerType<int>("Int");
+//
+//
+//    std::cout << "unsorted" << std::endl;
+//    for(int i=0; i < orders.size(); i++) {
+//        std::cout << orders[i] << std::endl;
+//
+//        for(int j=0; j < unsorted.size(); j++) {
+//            auto startInsertion = std::chrono::steady_clock::now();
+//            genogrove::IBPTree tree(orders[i]);
+//            std::cout << unsorted[j] << std::endl;
+//            std::ifstream infile(unsorted[j]);
+//            std::string line;
+//            while(std::getline(infile, line)) {
+//                std::istringstream iss(line);
+//                std::string chrom;
+//                int start, end, data;
+//                iss >> chrom >> start >> end >> data;
+//                genogrove::Interval intvl{size_t(start), size_t(end)};
+//                genogrove::Key key{intvl, data};
+//                tree.insert(chrom, key);
+//            }
+//            // time the insertion
+//            auto endInsertion = std::chrono::steady_clock::now();
+//            std::chrono::duration<double> insertionDuration = endInsertion - startInsertion;
+//            std::cout << "Time taken for insertion: " << insertionDuration.count() << " seconds\n\n";
+//        }
+//    }
+//
+//
+//    // time the search
+//
+//
+//
+//
+//
+//
 }
 
 
