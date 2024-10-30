18a808190c2e05fe247a6456ef6a7b8b7e4c6aac
riasc <richard.schaefer@zoho.com>
2024-10-29 22:03:03 -0500

    added file containing constants for library

---
+++ include/genogrove/Constants.hpp
@@ -0,0 +1,13 @@
+#ifndef GENOGROVE_CONSTANTS_HPP
+#define GENOGROVE_CONSTANTS_HPP
+
+// Standard
+#include <cstddef>
+
+namespace genogrove {
+    namespace constants {
+        static constexpr size_t BITS = 6; // 2^6 = 64 chromosomes (should be enough for now...)
+    }
+}
+
+#endif //GENOGROVE_CONSTANTS_HPP
