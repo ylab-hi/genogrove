0d7053762d9b3e8b70609402c656f9f6bb2cc5c1
riasc <richard.schaefer@zoho.com>
2024-10-30 16:13:00 -0500

    moved hits struct to datatypes

--- include/genogrove/DataTypes.hpp
+++ include/genogrove/DataTypes.hpp
@@ -1,8 +1,39 @@
 #ifndef IBPTREE_DATATYPES_HPP
 #define IBPTREE_DATATYPES_HPP
 
-namespace dtp {
-    using Interval = std::pair<size_t, size_t>; // interval (start, end)
+// Class
+#include "Interval.hpp"
+#include "AnyType.hpp"
+
+namespace genogrove {
+    namespace dtp {
+        // genomic coordinates
+        struct Coordinate {
+            std::string chromosome;
+            char strand;
+            Interval interval;
+
+            Coordinate(std::string chromosome, char strand, Interval interval) :
+                chromosome(chromosome), strand(strand), interval(interval) {}
+        };
+
+        struct Hit {
+            Coordinate coordinate;
+            std::shared_ptr<AnyBase> data;
+
+            Hit(Coordinate coordinate, std::shared_ptr<AnyBase> data) :
+                coordinate(coordinate), data(data) {}
+        };
+
+        struct Hits {
+            Coordinate query;
+            std::vector<Hit> hits;
+
+            Hits(Coordinate query) : query(query), hits(std::vector<Hit>()) {}
+            void addHit(Hit hit) { hits.push_back(hit); }
+        };
+    }
 }
+
 
 #endif //IBPTREE_DATATYPES_HPP
