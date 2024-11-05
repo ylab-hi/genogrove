4ff67cdea5fe7a1a57829a4373d8efca034f9fa3
Yangyang Li <yangyang.li@northwestern.edu>
2024-11-05 13:46:57 -0600

    Add reference link to nanobind documentation
    Included a link to the nanobind documentation for easier reference. This will help developers understand the basics of nanobind and its usage without needing to search for it separately.

--- python/ext.cpp
+++ python/ext.cpp
@@ -1,7 +1,10 @@
 #include <nanobind/nanobind.h>
 #include <genogrove/all.hpp>
 
+//https://nanobind.readthedocs.io/en/latest/basics.html
+
 int add(int a, int b) { return a + b; }
+
 
 NB_MODULE(genogrove, m) {
     m.def("add", &add);
