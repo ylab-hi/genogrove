aca90c5b7f2d0ddd0074211994e7d9e102060929
riasc <richard.schaefer@zoho.com>
2024-11-07 23:16:08 -0600

    cjange list

--- src/Chroms.cpp
+++ src/Chroms.cpp
@@ -3,25 +3,25 @@
 namespace genogrove {
     Chroms::Chroms() : chromosomes{}, nextValue{0} {}
 
-    std::bitset<BITS> Chroms::registerChrom(const std::string& chrom) {
+    std::bitset<constants::BITS> Chroms::registerChrom(const std::string& chrom) {
         auto it = chromosomes.find(chrom);
         if(it != chromosomes.end()) {
             return it->second; // chromosome is already registered
         }
 
         // create a new bitset (for the provided chromosome)
-        std::bitset<BITS> bits(nextValue);
+        std::bitset<constants::BITS> bits(nextValue);
         chromosomes[chrom] = bits;
         nextValue++;
         return bits;
     }
 
     // get the next available bitset (without registering the chromosome)
-    std::bitset<BITS> Chroms::getNextBitset() const {
-        return std::bitset<BITS>(nextValue);
+    std::bitset<constants::BITS> Chroms::getNextBitset() const {
+        return std::bitset<constants::BITS>(nextValue);
     }
 
-    std::bitset<BITS> Chroms::getBitset(std::string chrom) const {
+    std::bitset<constants::BITS> Chroms::getBitset(std::string chrom) const {
         auto it = chromosomes.find(chrom);
         if(it != chromosomes.end()) {
             throw std::runtime_error("Chromosome " + chrom + " not registered");
