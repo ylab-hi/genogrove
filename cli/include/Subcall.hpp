cf4a0df6f1a34bb0fc284d35251e3313e9282d6d
riasc <richard.schaefer@zoho.com>
2024-11-13 22:20:06 -0600

    added function for execute

--- cli/include/Subcall.hpp
+++ cli/include/Subcall.hpp
@@ -1,8 +1,13 @@
-//
-// Created by Richard Albin Schaefer on 11/12/24.
-//
-
 #ifndef GENOGROVE_SUBCALL_HPP
 #define GENOGROVE_SUBCALL_HPP
+
+#include "cxxopts.hpp"
+
+class Subcall {
+    public:
+        virtual cxxopts::ParseResult parseArgs(int argc, char** argv) = 0;
+        virtual void execute(const cxxopts::ParseResult& args) = 0;
+        virtual ~Subcall() = default;
+};
 
 #endif //GENOGROVE_SUBCALL_HPP
