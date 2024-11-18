a3034c1dd8ac5254ed603502fce0a456cda03fb6
riasc <richard.schaefer@zoho.com>
2024-11-18 13:04:21 -0600

    added struct to specify file entry to simpler processing

--- cli/include/FileEntry.hpp
+++ cli/include/FileEntry.hpp
@@ -1,8 +1,20 @@
-//
-// Created by Richard Albin Schaefer on 11/18/24.
-//
-
 #ifndef GENOGROVE_FILEENTRY_HPP
 #define GENOGROVE_FILEENTRY_HPP
+
+// Standard
+#include <string>
+
+// Class
+#include "genogrove/Interval.hpp"
+
+struct FileEntry {
+    std::string chrom;
+    genogrove::Interval interval;
+    char strand;
+
+    FileEntry() : chrom(""), interval(0, 0), strand('.') {}
+    FileEntry(std::string chrom, genogrove::Interval interval, char strand) :
+        chrom(chrom), interval(interval), strand(strand) {}
+};
 
 #endif //GENOGROVE_FILEENTRY_HPP
